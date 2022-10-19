5d63345660885fb1057278ea728d4c8a570d2822
petersjang1993 <petersjang1993@gmail.com>
2022-10-19 15:02:33 +0000

    almost there

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,16 @@ class UsersController < ApplicationController
 
     render({ :template => "user_templates/index.html.erb" })
   end
+
+  def show 
+    # Parameters: {"path_username" => "anisa" }
+    url_username = params.fetch("path_username")
+
+    matching_usernames = User.where({ :username => url_username })
+
+    @the_user = matching_usernames.first
+
+    render({ :template => "user_templates/show.html.erb" })
+  end 
+
 end
